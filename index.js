bf8c7a570a5153aa8019636b6a401172110be1d5
david-shakaya <devidoz666@gmail.com>
2021-01-18 14:12:47 +0200

    +

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log('yyyeap');
-console.log('yyyeap');
+// Имитирую обращение к бекенду, как бы обращаюсь с помощю fetch к файлу ZonAnn.Ts+dSST
+// для преобразования файла с форматом CSV используем метод text()
+fetch('./ZonAnn.Ts+dSST.csv')
+    .then(res => console.log(res.text()))
+    // .then(data => console.log(data))
 
