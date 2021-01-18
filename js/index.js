6a8eeb6985ed3fd8248d6022b2d077d5e13fbc03
david-shakaya <devidoz666@gmail.com>
2021-01-19 01:06:38 +0200

    +

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Имитирую обращение к бекенду, как бы обращаюсь с помощю fetch к файлу ZonAnn.Ts+dSST
 // для преобразования файла с форматом CSV используем метод text()
+
+//  Что бы распарсить CSV Удобно будем использовать библиотеку https://www.papaparse.com/
 fetch('./ZonAnn.Ts+dSST.csv')
     .then(res => res.text())
-    .then(data => console.log(data))
+    .then(data => console.log(Papa.parse(data)))
+
+
 
